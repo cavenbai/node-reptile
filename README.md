fea62a1347a8bd7a617616e861c861f28d722144
baipengfei02 <baipengfei02@megvii.com>
2020-06-16 16:29:33 +0800

    feat:ins

--- README.md
+++ README.md
@@ -9,7 +9,16 @@ node (脚本名)$1
 ```
 ## 项目说明
 ### 微博照片爬取
+```
 - 支持多人爬取：初始化 characterName = ['王宝抢','刘亦肥','刘的读'];
 - cookie初始化（自己在微博登录copy cookie到本地）
 - 下载的文件会根据characterName自动创建对应的图片文件夹
+```
 
+### instagram用户照片爬取
+```
+- 支持多用户爬取：初始化 characterName = ['王宝抢','刘亦肥','刘的读'];注：该名称为搜索准确人名
+- 若设置代理请求，初始化全局proxy 并在每处request添加 proxy:proxy。(请文明上网)
+- cookie初始化（登录ins账号copy cookie到本地）
+- 下载的文件会根据characterName自动创建对应的图片文件夹
+- query_hash参数不重要，随便找个就可以
--- ins_photo.js
+++ ins_photo.js
@@ -1,50 +1,67 @@
 const request = require('request-promise')
+const cheerio = require("cheerio")
 const fs = require('fs')
-
-var cookie = 'ig_cb=1; ig_did=5E08F81D-5F5F-4B06-8848-20974CB4DEF5; mid=XuTX3gALAAGhDIfiYZwgtmfvcNdi; csrftoken=Isx8Kcx8rmdYICXJ85JhcC8HZ9yupj8o; ds_user_id=9973078569; sessionid=9973078569%3A85ZJAMkl65TXzH%3A13; shbid=15643; shbts=1592055863.1224487; urlgen="{\\"178.128.186.198\\": 14061\\054 \\"161.35.39.229\\": 14061\\054 \\"133.18.208.83\\": 24282}:1jkr6J:TjKv66Bx_B1xV3WTLq-HJzttfGw"';
-var proxy = 'http://127.0.0.1:1080/pac?auth=S3rQWsbKhxZuIFvYyWzr&t=202006160000374167'
-var variablesParams = {"id":"5923076936","first":12,"after":"QVFBT0pnSGdoUDJiSFp5SGVQRG5pWUVoMlZmUWZuVkpjYVpSbXNhUnZvTEZQNFJ4U2V3S0RIRHF2RUY2Y2FfWl80N1RkVExWQ3RadTRPb2hiMVFnd3ZFQg=="}
-var option = {
-    query_hash: 'eddbde960fed6bde675388aac39a3657',
-    variables: ''
-}
-var page = 1
-const start = async () => {
+var cookie = 'xxx';
+var proxy = 'xxx'
+var header = {'cookie':cookie,'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+var variablesParams = {"id":"","first":12,"after":""}
+var option = {query_hash: 'eddbde960fed6bde675388aac39a3657',variables: ''}
+var page = 1 // 页数统计
+const start = async (name) => {
+    if (page === 1) { await homeData(name)}
     option.variables = JSON.stringify(variablesParams)
-    const responseData = await request({
-        url: 'https://www.instagram.com/graphql/query/',
-        qs: option,
-        // header: {cookie:cookie},
-        proxy: proxy,
-        json:true
-    });
+    const responseData = await request({ url: 'https://www.instagram.com/graphql/query/',qs: option,header: header,json:true});
     const responsData = responseData.data.user.edge_owner_to_timeline_media
+    const urls = []
+    responsData && responsData.edges.forEach( x => {
+        if (x.node.video_url) {urls.push(x.node.video_url)}
+        if (x.node.edge_sidecar_to_children) {
+            x.node.edge_sidecar_to_children.edges.forEach( m => { urls.push(m.node.display_url) })
+        } else urls.push(x.node.display_url)
+    })
+    console.log(`开始下载${name}第${page}页数据`)
+    for (let e of urls) {let r = await down(e,name);console.log(r)}
     if (responsData.page_info.has_next_page) {
-        variablesParams.after = responsData.page_info.end_cursor
-        let urls = []
-        responsData.edges.forEach( x => {
-            if (x.node.video_url) {urls.push(x.node.video_url)}
-            if (x.node.edge_sidecar_to_children) {
-                x.node.edge_sidecar_to_children.edges.forEach( m => {
-                    urls.push(m.node.display_url)
-                })
-            } else {
-                urls.push(x.node.display_url)
-            }
-        })
-        console.log(`开始下载第${page}页数据`)
-        for (let e of urls) {
-            let r = await down(e);
-            console.log(r)
-        }
+        variablesParams.after = responsData.page_info.end_cursor      
         page++;
-        start().then()
-    }
+        start(name)
+    } else {
+        page = 1
+        return
+    } 
 };
 
-const down = async (m) => {
+const homeData = async (name) => {
+    // 获取首页数据及ajax参数
+    const responseFirstData = await request.get(`https://www.instagram.com/${name}`,{headers:header}) 
+    const $ = cheerio.load(responseFirstData)
+    $('script').each(function(indexInArray) {    
+        if ($('script').eq(indexInArray).html().indexOf("window._sharedData") == 0) {    
+            let dataParams = $('script').eq(indexInArray).html()
+            let dataf = dataParams.substring(20)
+            let datae = JSON.parse(dataf.substring(0,dataf.length -1))
+            let endCursor = datae.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.page_info.end_cursor   
+            let userId = datae.entry_data.ProfilePage[0].graphql.user.id
+            variablesParams.id = userId
+            variablesParams.after = endCursor
+            option.variables = JSON.stringify(variablesParams)
+        }    
+    })  
+}
+
+const creatFile = async (screen_name) => {
+    return new Promise((resolve) => {
+        fs.exists(`./${screen_name}`, (exists) => {
+            if (!exists) {fs.mkdir(`./${screen_name}`,(err) =>{});resolve()}
+            if (exists) {resolve()}
+        });
+    })
+};
+
+const down = async (m,name) => {
+    await creatFile(name)
     return new Promise((resolve,reject) => {
-        request({url:m,proxy:proxy,}).pipe(fs.createWriteStream(`./img/${(new Date()).valueOf()}.${m.split('?')[0].split('.').pop()}`,{ 'enconding':'binary'})
+        request({url:m,headers:header}).pipe(fs.createWriteStream(`./${name}/${(new Date()).valueOf()}.${m.split('?')[0].split('.').pop()}`,{ 'enconding':'binary'})
             .on("error", (e) => {reject('下载失败:' + e)})
             .on("finish", () => {resolve("下载成功" + m);})
             .on("close", () => {})
@@ -52,7 +69,12 @@ const down = async (m) => {
     })
 };
 
+async function startUp () {
+    for (let i = 0; i < characterName.length; i++) {
+        await start(characterName[i])
+    }
+}
 
+const characterName = ['xiaomi.usa','kaven'];
 
-
-start().then();
+startUp().then();
